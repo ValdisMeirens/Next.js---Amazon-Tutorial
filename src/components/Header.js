0f26ccecc724d41aa3b4e7a167d57f2af810640a
Valdis Meirens <valdismeirens@gmail.com>
2022-07-20 09:50:54 +0300

    init

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+          />
+        </div>
+      </div>
+      <div></div>
+      <h1>HEADER</h1>
+    </header>
+  );
+}
+
+export default Header;
